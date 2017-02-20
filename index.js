4f08ef0f8cf34ac4fe3722166fcb6fd1162079d4
Nyceane <dotnetpete@gmail.com>
2017-02-19 23:55:01 -0800

    added drag and drop module

--- index.js
+++ index.js
@@ -3,12 +3,19 @@ var http = require('http');
 var fs = require("fs");
 var querystring = require('querystring');
 var request = require('request');
+var multer    =   require( 'multer' );
+var upload    =   multer( { dest: 'uploads/' } );
+var sizeOf    =   require( 'image-size' );
+var exphbs    =   require( 'express-handlebars' );
+require( 'string.prototype.startswith' );
+
 
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
+app.use(express.static( __dirname + '/bower_components' ) );
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
@@ -16,6 +23,48 @@ app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
   res.render('pages/index');
+});
+
+app.post( '/upload', upload.single( 'file' ), function( req, res, next ) {
+
+  if ( !req.file.mimetype.startsWith( 'image/' ) ) {
+    return res.status( 422 ).json( {
+      error : 'The uploaded file must be an image'
+    } );
+  }
+
+  var dimensions = sizeOf( req.file.path );
+
+  if ( ( dimensions.width < 640 ) || ( dimensions.height < 480 ) ) {
+    return res.status( 422 ).json( {
+      error : 'The image must be at least 640 x 480px'
+    } );
+  }
+
+    var post_data = {
+      'imageFile' : fs.createReadStream(req.file.path)
+    };
+
+    var options = {
+      url: 'http://api.emovu.com/api/image',
+      formData: post_data,
+      headers: {
+          'LicenseKey':'17228707618720079007733533670174311743221200521420042019733462300514011721'
+      }
+    };
+
+
+    request.post(options, function optionalCallback(err, httpResponse, body) {
+      if (err) {
+        res.send(err);
+        return console.error('upload failed:', err);
+      }
+      console.log('Upload successful!  Server responded with:', body);
+      res.send(body);
+    });
+    
+
+  //return res.status( 200 ).send( req.file );
 });
 
 app.get('/image', function(req, res) {
@@ -28,7 +77,7 @@ app.get('/image', function(req, res) {
       url: 'http://api.emovu.com/api/image',
       formData: post_data,
       headers: {
-          'LicenseKey':'[key]'
+          'LicenseKey':'17228707618720079007733533670174311743221200521420042019733462300514011721'
       }
     };
     
